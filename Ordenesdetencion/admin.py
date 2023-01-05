512123697117fa535490c0de40ba34f60722c596
Francisco <fpizarro@bluelatam.com>
2023-01-04 22:10:32 -0300

    Nuevo

---
+++ Ordenesdetencion/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from Ordenesdetencion.models import Actividad, Comuna, Delito, EstadoCivil, MedidaCautelar, Orden, Pais, Persona, Region, Sexo, Tribunal
+
+
+admin.site.register(Persona)
